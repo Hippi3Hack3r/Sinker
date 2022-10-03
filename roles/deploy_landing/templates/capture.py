e32cd1afdf32e9cdbe9bea4cab7038633e4f2f67
Hannah Cartier <cartier.hannah@utah.edu>
2022-10-03 10:05:25 -0600

    added ipino data, changed logging to json. Fixed a bug.

--- roles/deploy_landing/templates/capture.py
+++ roles/deploy_landing/templates/capture.py
@@ -1,19 +1,28 @@
 from flask import Flask, render_template, request, jsonify
 import json
+import ipinfo
+
 app = Flask(__name__)
 
-@app.route('/confirm.html', methods=['POST'])
+@app.route('/confirm', methods=['POST'])
 def foo():
+    file = open('/var/www/ipinfo.txt',mode='r')
+    token = file.read().rstrip()
+    file.close()
+    handler = ipinfo.getHandler(token)
+
     user_agent = request.headers.get('User-Agent', None)
     real_ip = request.headers.get('X-Real-Ip', None)
-    data = request.json
-    print(data)
-    entry = str(real_ip) + ',' + str(json.dumps(data)) + ',' + str(user_agent) + '\n'
-    f = open('forms.txt', 'a')
-    f.write(entry)
-    return render_template('success.html')
-    f.close()
 
+    details = handler.getDetails(real_ip)
+
+    data = request.json
+
+    merged_dict = {**data, **details.all}
+    json_merged = json.dumps(merged_dict)
+    print(json_merged)
+    with open('forms.json', 'w') as outfile:
+        outfile.write(json_merged)
 
 @app.errorhandler(404)
 # inbuilt function which takes error as parameter
--- runme_first.sh
+++ runme_first.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-if ! command -v ansible &> /dev/null ; then
-    install_ansible
-fi
-
 install_ansible() {
   # install ansible on local machine
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -15,3 +10,7 @@ install_ansible() {
     echo "unrecognized os. Exiting"
   fi
 }
+
+if ! command -v ansible &> /dev/null ; then
+    install_ansible
+fi
